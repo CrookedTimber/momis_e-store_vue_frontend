114752045d35f547efa66b6ab17b604f6c7a9e0f
Edgar Ruizcd <45479530+CrookedTimber@users.noreply.github.com>
2022-11-07 05:51:05 +0000

    first commit
    To be fixed: Stripe axios call

---
+++ src/components/ProductBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="column is-3">
+    <div class="box">
+      <figure class="image mb-4">
+        <img :src="product.get_thumbnail" />
+      </figure>
+      <h3 class="is-size-4">{{ product.name }}</h3>
+      <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+      <Router-link
+        v-bind:to="product.get_absolute_url"
+        class="button is-dark mt-4"
+        >View details</Router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductBox',
+  props: {
+    product: Object,
+  },
+};
+</script>
+
+<style scoped>
+.imgae {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
